b8bff0922219742c12a73e0efdfed93b797bdd8f
Eric Marrone <emarrone17@gmail.com>
2017-12-02 20:29:59 -0500

    Add action to ensure user owns picture to pictures controller

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -1,6 +1,7 @@
 class PicturesController < ApplicationController
   before_action :ensure_logged_in, except: [:show, :index]
   before_action :load_picture, only: [:show, :edit, :update, :destroy]
+  before_action :ensure_user_owns_picture, only: [:edit, :update, :destroy]
 
 
   def index
@@ -13,7 +14,12 @@ class PicturesController < ApplicationController
   def load_picture
     @picture = Picture.find(params[:id])
   end
-
+  def ensure_user_owns_picture
+    unless current_user == @picture.user
+      flash[:alert] = "Please log in"
+      redirect_to new_sessions_url
+    end
+  end
   def show
 
   end
