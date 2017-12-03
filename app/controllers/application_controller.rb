4820cfdf28af3ff34623540cff18344f3fa7395b
Eric Marrone <emarrone17@gmail.com>
2017-12-02 19:40:17 -0500

    Add authorization action to applications controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,13 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+  def ensure_logged_in
+    unless current_user
+      flash[:alert] = "Please login"
+      redirect_to new_sessions_url
+    end
+  end
+
   private
   helper_method :current_user
   def current_user
